0aa67b5590da09ac4844da1ea2e677b91e78b11b
shigeru.nakajima <shigeru.nakajima@luxiar.com>
2017-01-05 10:32:51 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# What's this
+
+これは週末の掃除の時間を告げるAWS Lambda関数です。
+
+# Configurable
+
+## 環境変数
+
+|key|value|
+|---|-----|
+| ENCRYPTED_CHATWORK_API_TOKEN |KMSで暗号化したChatwork API Token|
+| CHATWORK_ROOM_ID | 投稿したいChatworkのRoom ID（数字のみ）|
+
+# How To Deploy
+
+## refs
+[AWS LambdaからChatworkに定時通知する - Qiita](http://qiita.com/ledsun/items/cc90978d0e09f459d571)を参考にしてください。
+
+圧縮ファイルは`npm run zip`で作成可能です。
+
+## cron expression
+
+`0 9 ? * FRI *`
---
+++ index.js
@@ -0,0 +1,34 @@
+const AWS = require('aws-sdk')
+const postChatworkMessage = require('post-chatwork-message')
+
+const encryptedToken = process.env.ENCRYPTED_CHATWORK_API_TOKEN
+const message = `MDCにいる方は18時から掃除です。宜しくお願いします。
+
+掃除
+- 床を拭く
+- ゴミを集める
+- トイレ掃除
+  - 次の補充、全て掃除用具箱の中に入っています
+  - トイレットペーパー
+  - 手を拭く紙 (名前ど忘れしました.....)
+  - 消臭力、消臭スプレー
+- 灰皿を洗う
+- 水とコーヒーの受け皿を洗う
+- ゴミを一回に持っていく
+- 使い終わったカップを元の位置に戻す
+- カフェと会議室の机、棚拭き`
+
+exports.handler = function(event, context) {
+  new AWS.KMS({
+      region: 'ap-northeast-1'
+    })
+    .decrypt({
+      CiphertextBlob: new Buffer(encryptedToken, 'base64')
+    }, (err, data) => {
+      if (err) {
+        console.log(err)
+        return context.fail(err)
+      }
+      postChatworkMessage(data.Plaintext.toString(), process.env.CHATWORK_ROOM_ID, message)
+    })
+}
